aa5fdc4554da36934c45d5db0297d0d3be098ebd
Rodrigo C. Garcia <rodiza082003@hotmail.com>
2025-11-01 12:24:10 -0300

    commit

--- src/src/Main.java
+++ src/src/Main.java
@@ -7,6 +7,7 @@ import br.com.davidbuzatto.jsge.geom.Rectangle;
 import br.com.davidbuzatto.jsge.image.Image;
 import br.com.davidbuzatto.jsge.imgui.GuiButton;
 import arvores.ArvoreBinaria;
+import br.com.davidbuzatto.jsge.imgui.GuiInputDialog;
 import br.com.davidbuzatto.jsge.math.Vector2;
 
 /**
@@ -16,17 +17,23 @@ import br.com.davidbuzatto.jsge.math.Vector2;
  */
 public class Main extends EngineFrame {
     
-    private GuiButton adicionarElemento;
-    //arve
+    private GuiButton btnAddElemento;
+    private GuiInputDialog inputAddElemento;
+    //arvere
     private ArvoreBinaria<Integer, String> arvore;
     private Vector2 posicao;
+    
+    //A arvore tem uma chave e um valor, só a chave é comparada
+    //quando vai decidir pra que lado a node vai, então o valor
+    //vou tentar deixar uma string vazia como padrao pra ver no que da
+    private String valorPadrao = "";
     
     
     public Main() {
         
         super(
-            800,                 // largura                      / width
-            450,                 // algura                       / height
+            1000,                 // largura                      / width
+            600,                 // algura                       / height
             "Visualizador de Árvore Binária",      // título                       / title
             60,                  // quadros por segundo desejado / target FPS
             true,                // suavização                   / antialiasing
@@ -45,32 +52,92 @@ public class Main extends EngineFrame {
         arvore = new ArvoreBinaria<>();
         posicao = new Vector2(100, 200);
         
-        arvore.put( 5, "cinco" );
-        arvore.put( 2, "dois" );
-        arvore.put( 10, "dez" );
-        arvore.put( 15, "quinze" );
-        arvore.put( 12, "doze" );
-        arvore.put( 1, "um" );
-        arvore.put( 3, "três" );
+        btnAddElemento = new GuiButton(430, 100, 140, 40, "Adicionar Elemento", this);
+        tratarBotao(btnAddElemento);
+        
+        inputAddElemento = new GuiInputDialog("Inserir Elemento", 
+                "Insira um elemento na árvore: ", 
+                true, this);
+        tratarInputBox(inputAddElemento);
+        
     }
 
 
     @Override
     public void update( double delta ) {
+        btnAddElemento.update(delta);
+        inputAddElemento.update(delta);
+        
+        if(btnAddElemento.isMousePressed()){
+            inputAddElemento.show();
+        }
+        logicaInput(inputAddElemento);
         
     }
     
 
     @Override
     public void draw() {
-        drawNode(arvore, posicao);
-        
-    
+        btnAddElemento.draw();
+        inputAddElemento.draw();
     }
     
     private void drawNode(ArvoreBinaria<Integer, String> tree, Vector2 pos ){
         drawCircle(pos, 40, BLACK);
         drawText(tree.get(5), pos, PINK);
+    }
+    
+    
+    //Isso aqui é só pra diminuir a quantidade de código no create
+    //Só vai deixar o botao mais bonitinho
+    private void tratarBotao(GuiButton btn){
+        btn.setBackgroundColor(DARKGREEN);
+        btn.setTextColor(BLACK);
+        btn.setBorderColor(BLACK);
+    }
+    
+    //Mesmo que a função de cima só que para input box
+    private void tratarInputBox(GuiInputDialog caixa){
+        caixa.setBorderColor(BLACK);
+        caixa.setTitleBarBackgroundColor(DARKGREEN);
+        caixa.setTitleBarTextColor(BLACK);
+        caixa.setTextColor(BLACK);
+    }
+    
+    //Também para diminuir qntd de código no update
+    //Só vai checar os botoes de ok, cancelar, etc
+    private void logicaInput(GuiInputDialog caixa){
+        int valor = 0;
+        
+        if ( caixa.isCloseButtonPressed() || caixa.isCancelButtonPressed() ) {
+            caixa.hide();
+        }
+        
+        if(ehInt(caixa.getValue())){
+            valor = Integer.parseInt(caixa.getValue());
+        } else{
+            caixa.setTitleBarBackgroundColor(RED);
+            caixa.setText("Precisa ser um número");
+        }
+        
+        if(caixa.isEnterKeyPressed() || caixa.isOkButtonPressed()){
+            arvore.put(valor, valorPadrao);
+            caixa.hide();
+        }
+        
+        drawNode(arvore, posicao);
+        
+    }
+    
+    
+    //Checa que se é a string é um numero
+    private boolean ehInt(String string){
+        try {
+            Integer.parseInt(string);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
     
     public static void main( String[] args ) {
